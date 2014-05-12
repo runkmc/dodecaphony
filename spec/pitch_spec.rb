ba167399b5002a453071ba94fb877cd52848732e
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-11 22:16:53 -0700

    Useless test eliminated

--- spec/pitch_spec.rb
+++ spec/pitch_spec.rb
@@ -2,12 +2,6 @@ require 'pitch'
 
 describe Pitch do
 
-  it "exists" do
-    pitch = Pitch.new "C#"
-
-    expect(pitch).to be_kind_of Pitch
-  end
-
   it "can tell you its pitch name" do
     pitch = Pitch.new "A"
 
