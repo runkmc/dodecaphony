56e1ce48f7edf250068a922378dc033ae5a2e1e1
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-12 16:38:40 -0700

    Dodecaphony can respell its row using sharps

--- lib/dodecaphony.rb
+++ lib/dodecaphony.rb
@@ -1,13 +1,35 @@
+require_relative 'pitch'
+
 class Dodecaphony
 
   attr_reader :original_row
 
-  def initialize tone_row
+  def initialize tone_row, spelling=:sharps
     self.original_row = tone_row
+    self.normalized_row = normalize_row spelling: spelling
+  end
+
+  def p0
+    normalized_row
   end
 
   private
 
-  attr_writer :original_row
+  def normalize_row sharps=true
+    row = get_pitches
+    row.each_with_object([]) do |pitch, a|
+      a << (sharps ? pitch.spell_as_sharp : pitch.spell_as_flat)
+    end
+  end
+
+  def get_pitches
+    original_row.each_with_object([]) do |pitch_name, a|
+      a << Pitch.new(pitch_name)
+    end
+  end
+
+  attr_reader :normalized_row
+
+  attr_writer :original_row, :normalized_row
 
 end
--- spec/dodecaphony_spec.rb
+++ spec/dodecaphony_spec.rb
@@ -8,4 +8,12 @@ describe Dodecaphony do
 
     expect(new_dod.original_row).to eq tone_row
   end
+
+  it "can respell the row, favoring sharps" do
+    tone_row = %w[ c c+ d d# fb f gb g a- b-- bb b ]
+    new_dod = Dodecaphony.new tone_row, spelling: :favor_sharps
+
+    expect(new_dod.p0).to eq %w[ C C# D D# E F F# G G# A A# B ]
+  end
+
 end
