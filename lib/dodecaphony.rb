0e4e696fcdefd47cd0e154567bdd17548eb0e68d
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-12 15:59:16 -0700

    Implement original_row

--- lib/dodecaphony.rb
+++ lib/dodecaphony.rb
@@ -1,3 +1,13 @@
 class Dodecaphony
 
+  attr_reader :original_row
+
+  def initialize tone_row
+    self.original_row = tone_row
+  end
+
+  private
+
+  attr_writer :original_row
+
 end
--- spec/dodecaphony_spec.rb
+++ spec/dodecaphony_spec.rb
@@ -2,7 +2,7 @@ require 'dodecaphony'
 
 describe Dodecaphony do
 
-  xit "initializes with an array of strings and returns the same value" do
+  it "initializes with an array of strings and returns the same value" do
     tone_row = %w[ a a# b c db d eb e f f# g g# ]
     new_dod = Dodecaphony.new tone_row
 
