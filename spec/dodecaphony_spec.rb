4d6cd31d840309491de00382fa30b1c8aa1eadbd
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-12 15:57:57 -0700

    Remove useless test

--- spec/dodecaphony_spec.rb
+++ spec/dodecaphony_spec.rb
@@ -2,9 +2,10 @@ require 'dodecaphony'
 
 describe Dodecaphony do
 
-  it "exists" do
-    new_dod = Dodecaphony.new 
+  xit "initializes with an array of strings and returns the same value" do
+    tone_row = %w[ a a# b c db d eb e f f# g g# ]
+    new_dod = Dodecaphony.new tone_row
 
-    expect(new_dod).to be_kind_of Dodecaphony
+    expect(new_dod.original_row).to eq tone_row
   end
 end
