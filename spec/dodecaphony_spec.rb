663e628b2c0092bd8be9b6785aae51e98d214608
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-14 12:21:35 -0700

    Add test for #spell_with_sharps

--- spec/dodecaphony_spec.rb
+++ spec/dodecaphony_spec.rb
@@ -6,14 +6,14 @@ describe Dodecaphony do
     tone_row = %w[ a a# b c db d eb e f f# g g# ]
     new_dod = Dodecaphony.new tone_row
 
-    expect(new_dod.original_row).to eq tone_row
+    expect(new_dod.p0).to eq tone_row
   end
 
   it "can respell the row, favoring sharps" do
     tone_row = %w[ c c+ d d# fb f gb g a- b-- bb b ]
     new_dod = Dodecaphony.new tone_row, spelling: :favor_sharps
 
-    expect(new_dod.p0).to eq %w[ C C# D D# E F F# G G# A A# B ]
+    expect(new_dod.spell_with_sharps).to eq %w[ C C# D D# E F F# G G# A A# B ]
   end
 
 end
