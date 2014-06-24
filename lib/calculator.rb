03a9b36f7640f046f5fae44cd17b63c157cf32e6
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-06-23 19:42:42 -0700

    Start to move calculations into Calculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,6 +4,32 @@ module Dodecaphony
   class Calculator
 
     def initialize row
+      self.row = row
+    end
+
+    def p0
+      row
+    end
+
+    (0..11).each do |i|
+      define_method "p#{i}".to_sym do
+        transposed_row = row.pitches.each_with_object([]) { |pitch, new_row|
+          new_row << row.intervals[(transpose i, pitch)].name
+        }.to_a 
+        Dodecaphony::Row.new transposed_row
+      end
+    end
+
+    private
+
+    attr_accessor :row
+
+    def transpose interval, pitch
+      (starting_pitch.distance_from(pitch) + interval) % 12
+    end
+
+    def starting_pitch
+      row.pitches[0]
     end
 
   end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,11 +2,27 @@ require 'calculator'
 require 'ostruct'
 
 describe Dodecaphony::Calculator do
+
   it "initializes with a row" do
     row = Dodecaphony::Row.new %w[a b b- c c# d d# e f gb g g#]
     calc = Dodecaphony::Calculator.new row
     
     expect(calc).to be_kind_of(Dodecaphony::Calculator)
+  end
+
+  xit "can provide the original row" do
+    row = Dodecaphony::Row.new %w[a b b- c c# d d# e f gb g g#]
+    calc = Dodecaphony::Calculator.new row
+
+    expect(calc.p0).to eq row
+  end
+
+  xit "can provide p1" do
+    row = Dodecaphony::Row.new %w[ bb b c c# d f e eb g gb a ab ]
+    p1_row = Dodecaphony::Row.new %w[ b c c# d eb gb f e ab g bb a ]
+    calc = Dodecaphony::Calculator.new row
+
+    expect(calc.p1.to_s).to eq p1_row.to_s
   end
 
 end
