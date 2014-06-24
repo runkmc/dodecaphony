b12b4b7d015c3be885aa81003aeedd3418d5d722
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-06-23 18:34:36 -0700

    Dodecaphony::Calculator exists

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,5 +3,8 @@ require_relative 'row'
 module Dodecaphony
   class Calculator
 
+    def initialize row
+    end
+
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,5 +2,11 @@ require 'calculator'
 require 'ostruct'
 
 describe Dodecaphony::Calculator do
+  it "initializes with a row" do
+    row = Dodecaphony::Row.new %w[a b b- c c# d d# e f gb g g#]
+    calc = Dodecaphony::Calculator.new row
+    
+    expect(calc).to be_kind_of(Dodecaphony::Calculator)
+  end
 
 end
