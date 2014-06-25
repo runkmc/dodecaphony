95638a403ad41b3732b78c42574f1e18bc6ce199
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-06-24 19:45:49 -0700

    Move calculations out of Row for good and include Enumerable in row

--- lib/row.rb
+++ lib/row.rb
@@ -3,6 +3,7 @@ require 'set'
 
 module Dodecaphony
   class Row
+    include Enumerable
 
     attr_reader :original_row, :intervals
 
@@ -39,44 +40,9 @@ module Dodecaphony
       end
     end
 
-    # def i0
-    #   original_row.each_with_object([]) do |pitch, row|
-    #     row << intervals[((row_with_intervals.key(pitch) - 12).abs)].name
-    #   end
-    # end
-    #
-    # (0..11).each do |i|
-    #   define_method "p#{i}".to_sym do
-    #     original_row.each_with_object([]) do |pitch, row|
-    #       row << intervals[(transpose i, pitch)].name
-    #     end
-    #   end
-    # end
-    #
-    # (1..11).each do |i|
-    #   define_method "i#{i}".to_sym do
-    #     corresponding_p = self.send("p#{i}")
-    #     new_row = self.class.new corresponding_p
-    #     new_row.i0
-    #   end
-    # end
-    #
-    # (0..11).each do |i|
-    #   define_method "r#{i}".to_sym do
-    #     self.send("p#{i}".to_sym).reverse.each_with_object([]) do |pitch, row|
-    #       row << pitch
-    #     end
-    #   end
-    # end
-    #
-    # (0..11).each do |i|
-    #   define_method "ri#{i}".to_sym do
-    #     self.send("i#{i}").reverse.each_with_object([]) do |pitch, row|
-    #       row << pitch
-    #     end
-    #   end
-    # end
-
+    def each &block
+      pitches.each &block
+    end
     private
 
     attr_writer :intervals, :original_row
